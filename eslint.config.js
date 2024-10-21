ade0b7ecc86a31821f65bdb13a651c1dd33e4d3e
MBP <mino727@c-able.ne.jp>
2024-10-21 16:21:03 +0900

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,21 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import js, { configs } from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
-
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
-  skipFormatting,
-]
+export default {
+  files: [
+    {
+      name: 'app/files-to-lint',
+      files: ['**/*.{js,mjs,jsx,vue}'],
+    },
+    {
+      name: 'app/files-to-ignore',
+      ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    },
+  ],
+  configs: [
+    js.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    skipFormatting,
+  ],
+}
