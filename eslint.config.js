8c486baf26e84a4d4951891f66ac0268e05f4cfd
MBP <mino727@c-able.ne.jp>
2024-10-23 16:19:22 +0900

    ECMA SCRIPT 2015

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,6 @@ export default [
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
@@ -19,15 +18,19 @@ export default [
   ...pluginVue.configs['flat/recommended'],
   skipFormatting,
   {
+    languageOptions: {
+      ecmaVersion: 2015, // ECMAScript 2015 compatibility
+      sourceType: 'module', // Supports ES6 module syntax
+    },
     rules: {
       'padding-line-between-statements': [
         'error',
-        { blankLine: 'always', prev: 'import', next: '*' },  // インポート文の次に改行
+        { blankLine: 'always', prev: 'import', next: '*' },
       ],
-      'indent': ['error', 2],  // インデントはスペース2つ
-      'semi': ['error', 'always'],  // セミコロンを常に使う
-      'quotes': ['error', 'single'],  // シングルクォートを使う
-      'no-unused-vars': ['warn'],  // 使われていない変数は警告
+      indent: ['error', 2],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': ['warn'],
     },
   },
 ];
