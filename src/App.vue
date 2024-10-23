8abe75bf315ea367eea4fd7b48e688e31fe36ba6
MBP <mino727@c-able.ne.jp>
2024-10-23 17:55:49 +0900

    shadow

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,6 @@ import BaseCard from '@/components/BaseCard.vue';
 <template>
   <main class="flex-row text-center space-y-2 items-center justify-center min-h-screen">
   <h1 class="text-red-400">slots</h1>
+<BaseCard />
   </main>
 </template>
--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-
+<h1 class="shadow-lg">hello</h1>
 </template>
 
 <style scoped>
