d03e953d017f8822fe6879dca9330137cf9f44bc
iMac <mino727@c-able.ne.jp>
2024-10-27 11:36:49 +0900

    shallowref

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
-import BaseCard from '@/components/BaseCard.vue'
+import BaseCard from '@/components/BaseCard.vue';
 
-import CompA from '@/components/CompA.vue'
+import CompA from '@/components/CompA.vue';
 
-import CompB from '@/components/CompB.vue'
+import CompB from '@/components/CompB.vue';
 
-import { shallowRef } from 'vue'
+import { shallowRef } from 'vue';
 
-const currentComp = shallowRef(CompA)
-cost tempObj = shallowRef({
+const currentComp = shallowRef(CompA);
+const tempObj = shallowRef({
   count: 1,
-})</sript>
+});
+</script>
 <template>
   <main class="flex-row space-y-2 items-center justify-center min-h-screen">
     <h1 class="text-4xl">slots</h1>
@@ -57,7 +58,7 @@ cost tempObj = shallowRef({
     {{ tempObj.count }}
     <button
       class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-      @click='tempObj.count++'
+      @click="tempObj.count++"
     >
       +
     </button>
